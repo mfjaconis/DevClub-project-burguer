6cef51aed55b644b5cdbb23992bc293ea6fd5511
mfjaconis <matheusjaconis@gmail.com>
2023-09-27 17:35:50 -0300

    base do projetoo

---
+++ assets/js/scripts.js
@@ -0,0 +1,13 @@
+console.log(menuOptions)
+
+const btnforEach = document.querySelector('#forEach')
+const btnforMap = document.querySelector('#map')
+const btnReduce = document.querySelector('#reduce')
+const btnFilter = document.querySelector('#filter')
+
+btnforEach.addEventListener('click', () => {
+    const newMenuOptions = menuOptions.forEach(options => {
+
+    })
+})
+
